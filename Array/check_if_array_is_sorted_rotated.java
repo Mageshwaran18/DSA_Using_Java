177e24e783274fdcc44b1e34d13a832526316eab
Mageshwaran18 <mageshwaranp18@gmail.com>
2024-10-15 21:37:02 +0530

    Check if an array is sorted and rotated

---
+++ Array/check_if_array_is_sorted_rotated.java
@@ -0,0 +1,20 @@
+package Array;
+
+
+/* Leetcode No:- 1752 */
+
+/*
+ * Optimal approach --> In a sorted array ( non - decreasing order ) , consider i am traversing from the backside . Then the arr[i] > arr[i-1] , if the condition fails , it shows that it's not sorted properly .
+ */
+
+
+
+public class check_if_array_is_sorted_rotated {
+
+
+
+    public static void main(String[] args) {
+        
+    }
+    
+}

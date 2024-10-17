da936c7609cef5d8c458f5821147ec18be2b04d9
Mageshwaran18 <mageshwaranp18@gmail.com>
2024-10-17 14:31:14 +0530

    17.10.24

--- Sorting/bubble_sort.java
+++ Sorting/bubble_sort.java
@@ -29,13 +29,21 @@ public class bubble_sort {
     }
 
     public static void main(String[] args) {
+        int[] arr = {64, 34, 25, 12, 22, 11, 90};
+        System.out.println("Original array:");
+        printArray(arr);
         
-        int arr [] = {38, 27, 43, 3, 9, 82, 10, 56, 19, 71};
         bubble_sort(arr);
-        for(int i = 0 ; i < arr.length ; i++)
-        {
+        
+        System.out.println("Sorted array:");
+        printArray(arr);
+    }
+    
+    public static void printArray(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + " ");
         }
+        System.out.println();
     }
     
 }
--- Sorting/selection_sort.java
+++ Sorting/selection_sort.java
@@ -30,13 +30,21 @@ public class selection_sort {
     }
 
     public static void main(String[] args) {
-        
-        int arr [] = {64, 34, 25, 12, 22, 11, 90};
+        int[] arr = {64, 34, 25, 12, 22, 11, 90};
+        System.out.println("Original array:");
+        printArray(arr);
+  
         selection_sort(arr);
-        for(int i = 0 ; i < arr.length ; i++)
-        {
+  
+        System.out.println("Sorted array:");
+        printArray(arr);
+    }
+
+    public static void printArray(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + " ");
         }
+        System.out.println();
     }
     
 }

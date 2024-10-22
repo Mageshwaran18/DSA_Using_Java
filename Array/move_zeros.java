cb6423aac83de429c3cabd2b915988cdd1eb1a00
Mageshwaran18 <mageshwaranp18@gmail.com>
2024-10-22 14:20:41 +0530

    Move Zeros

--- Array/move_zeros.java
+++ Array/move_zeros.java
@@ -5,29 +5,37 @@ package Array;
 
 /*
  * My Approach --> I just tried to implement the remove duplicates from array inplace code with some changes
+ * 
+ * Optimal Approach --> Choose the first zero element (j)
+ *                  --> We know that , what ever the non zero element comes (from j+1 ) it should be before the first zero element 
+ *                  -- Time complexity --> O(n)
  */
 public class move_zeros {
 
     public static int[] moveZeros(int n, int []nums) {
-        
-        
-        
+    
+        int j = -1;
         for(int i = 0 ; i < n ; i++)
         {
             if(nums[i]==0)
             {
-                int j = i;
-                while( j < n && nums[j]==0)
-                {
-                    j++;
-                }
-                if( j < n)
-                {
-                    nums[i]=nums[j];
-                    nums[j]=0;
-                }
+                j = i;
+                break;
             }
         }
+
+        for(int i = j + 1 ; i < n ; i++)
+        {
+            if(nums[i]!=0)
+            {
+                int temp = nums[i];
+                nums[i] = nums[j];
+                nums[j] = temp;
+                j++;
+            }
+
+        }
+
         return nums;
     }
     

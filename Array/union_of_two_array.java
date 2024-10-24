92ac0856996e1764751704663b394be40b1183f6
Mageshwaran18 <mageshwaranp18@gmail.com>
2024-10-24 18:22:29 +0530

    Union of Two Array

--- Array/union_of_two_array.java
+++ Array/union_of_two_array.java
@@ -1,21 +1,21 @@
 package Array;
 import java.util.*;
 
-/* Brute Force approach --> Store the elements in the hashset which doesn't allow duplicates and it will sorted automatically 
+/* Brute Force approach --> Store the elements in the hashset which doesn't allow duplicates
  * 
- * Better approach --> Two pointer approach 
+ * Better approach --> Two pointer approach , it's as same as the merge two sorted linked list
+ *                 --> See the code , don't make the mistake twice 
+ *                 --> The problem is so simple don't make it complicate by thinking overly 
+ *                 --> Solve it in easy approach
  */
 
 
 public class union_of_two_array {
       public static List<Integer> union(int a[],int b[])
-      {
-          ArrayList<Integer> ans = new ArrayList<>();
-        
-          /*
+      { 
+          /* 
          * HashMap allows duplicate values but unique keys
          * HashSet only stores unique elements (no duplicates allowed)
-         */
           HashSet<Integer> set = new HashSet<>();
 
           int n1 = a.length;
@@ -30,10 +30,55 @@ public class union_of_two_array {
               set.add(b[j]);
           }
 
-          /* is as same as --> for(Integer st: set) ans.add(st) */
+         is as same as --> for(Integer st: set) ans.add(st) 
           ans.addAll(set);
+           */
+        
+        ArrayList<Integer> ans = new ArrayList<>();
+        int n1 = a.length;
+        int n2 = b.length;
+        int i = 0 , j = 0;
+        
+        while( i < n1 && j < n2)
+        {
+            if(a[i]<=b[j])
+            {
+                if(ans.size()==0||ans.get(ans.size()-1)!=a[i])
+                {
+                    ans.add(a[i]);
+                }
+                i++;
+            }
+            else
+            {
+                if(ans.size()==0 || ans.get(ans.size()-1)!=b[j])
+                {
+                    ans.add(b[j]);
+                }
+                j++;
+            }
+        }
 
-          return ans;
+        while( i < n1)
+        {
+            if(ans.size()==0 || ans.get(ans.size()-1)!=a[i])
+            {
+                ans.add(a[i]);
+            }
+            i++;
+        }
+
+        while( j < n2)
+        {
+            if(ans.size()==0 || ans.get(ans.size()-1)!=b[j])
+            {
+                ans.add(b[j]);
+            }
+            j++;
+        }
+
+        return ans;
+
     }
     
     public static void main(String[] args) {

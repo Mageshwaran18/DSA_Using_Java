b5454f3049c69bfd7aa6bb4be0f6613029dba044
Mageshwaran <mageshwaranp18@gmail.com>
2025-01-11 22:56:24 +0530

    Subarray product less than K

---
+++ Sliding_Window/subarray_product_less_than_k.java
@@ -0,0 +1,33 @@
+package Sliding_Window;
+
+/* LeetCode :- 713 https://leetcode.com/problems/subarray-product-less-than-k/description/ */
+
+/* Optimal Approach :- --> It's just an normal sliding window approach , easy to implement as we know the constraints for the window 
+ * 					   --> The number of elements in the window is need to be added to the cnt . ( Refer DSA Notes )
+*/
+
+public class subarray_product_less_than_k {
+    public static  void subarray_product_less_than_k(int[] nums , int k)
+	{
+		int l = 0 ; 
+        int pro = 1 , cnt = 0;
+        for(int r = 0 ; r < nums.length ; r++)
+        {
+            pro*=nums[r];
+            while(pro >= k)
+            {
+                pro/=nums[l];
+                l++;
+            }
+            cnt+=(r-l+1);
+		}
+
+		System.out.println(cnt);
+    }
+    
+    public static void main(String[] args) {
+        int[] nums = {10, 5, 2, 6};
+        int k = 100;
+        subarray_product_less_than_k(nums, k);
+    }
+}

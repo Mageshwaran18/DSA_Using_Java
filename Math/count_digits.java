83ad548117ad3c7e79b4b193520ced4fc7daaeba
Mageshwaran <mageshwaranp18@gmail.com>
2025-01-27 13:55:07 +0530

    Count Digits

---
+++ Math/count_digits.java
@@ -0,0 +1,21 @@
+package Math;
+
+class count_digits
+{
+    public static int count_digits(int n)
+    {
+        int count = 0;
+        while(n!=0)
+        {
+            n = n/10;
+            count++;
+        }
+        return count;
+    }
+
+    public static void main(String args[])
+    {
+        int k =1234;
+        System.out.println(count_digits(k));
+    }
+}

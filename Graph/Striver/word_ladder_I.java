fc972a70a3918f7ce8cb6e4af8d60435136985bd
Mageshwaran <mageshwaranp18@gmail.com>
2025-02-14 22:47:40 +0530

    Word Ladder I

---
+++ Graph/Striver/word_ladder_I.java
@@ -0,0 +1,70 @@
+package Graph.Striver;
+
+/* LeetCode :- 127 https://leetcode.com/problems/word-ladder/description/ */
+
+import java.util.*;
+
+class word_ladder_I
+{
+    static class Pair
+    {
+        String str;
+        int level;
+        public Pair(String str,int level)
+        {
+            this.str=str;
+            this.level=level;
+        }
+    }
+    public static int word_ladder_I(String bw, String ew, List<String> wordlist)
+    {
+        Queue<Pair> q = new LinkedList<>();
+        Set<String> wl = new HashSet<>();
+        for(String str: wordlist)
+        {
+            wl.add(str);
+        }
+        Set<String> visited = new HashSet<>();
+        q.add(new Pair(bw,1));
+        while(!q.isEmpty())
+        {
+            Pair temp = q.poll();
+            String par = temp.str;
+            int par_lev = temp.level;
+            visited.add(par);
+            for(int i = 0 ; i < par.length() ; i++)
+            {
+                for(int j = 97 ; j <= 122 ; j++)
+                {
+                    char c = (char)j;
+                    String chi = par.substring(0,i) + c + par.substring(i+1);
+                    if(wl.contains(chi) && !visited.contains(chi))
+                    {
+                        // System.out.println(chi);
+                        if(chi.equals(ew))
+                        {
+                            return par_lev+1;
+                        }
+                        else
+                        {
+                            q.add(new Pair(chi,par_lev+1));
+                            visited.add(chi);
+                        }
+                    }
+
+                }
+            }
+        }
+
+        return 0;
+    }
+
+    public static void main(String[] args) {
+        String beginWord = "hit";
+        String endWord = "cog";
+        List<String> wordList = Arrays.asList("hot","dot","dog","lot","log","cog");
+        
+        int result = word_ladder_I(beginWord, endWord, wordList);
+        System.out.println("Length of shortest transformation sequence: " + result);
+    }
+}

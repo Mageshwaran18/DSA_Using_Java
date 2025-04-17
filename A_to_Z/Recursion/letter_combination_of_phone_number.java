fc694b3ef335d6cfa41f269008ea45e6a895b601
Mageshwaran <mageshwaranp18@gmail.com>
2025-04-17 22:32:39 +0530

    17.4.25

--- A_to_Z/Recursion/letter_combination_of_phone_number.java
+++ A_to_Z/Recursion/letter_combination_of_phone_number.java
@@ -7,6 +7,24 @@ import java.util.*;
  *                      --> Where we will add that at first and perform DFS , then the result will be added to the list 
  *                      --> Then we will be removing that and repeat the process !
  *                      --> It's look like [ we two option either you add or remove ].
+ * 
+ *                  T.C :- if a string with 2345 where all the numbers have 3 alphabets then the result array will have 81 which 3^4 ( where 4 is the length of the string )
+ *                         if a string with 79 where all the numbers have 4 alphabets then the result array will have 16 which 4^2 ( where 2 is the length of the string )
+ *                         if all the numbers belongs to 3 alphabets then it's TC is 3^n
+ *                         if all the numbers belongs to 4 alphabets then it's TC is 4^n
+ *                         
+ *                         if the string is 73  then the result array will have 12 , if the string 737 then the result array will be 48
+ *                         if the string is 7373 then the result array will have 144 , So from that we can find some pattern 
+ *                         let the string 7373 can be also written in 77 33 [ applicable only for calculating the TC ] 
+ *                         T.C for 77 -> 4^2 = 16
+ *                         T.C for 33 -> 3^2 = 9
+ *                         
+ *                         At last the 16 strings will be multiplied into 9 strings .
+ *                         
+ *                         From this we can conclude that T.C would be O(3^n * 4^m) 
+ *                         -> where n is the count of 3 valued number and m is the count of 4 valued number
+ *                          
+ * 
 */
 
 public class letter_combination_of_phone_number {
--- A_to_Z/SlidingWindow_and_TwoPointers/Strivers/max_consecutive_one_III.java
+++ A_to_Z/SlidingWindow_and_TwoPointers/Strivers/max_consecutive_one_III.java
@@ -3,6 +3,13 @@ import java.util.*;
 
 /* LeetCode :- 1004 https://leetcode.com/problems/max-consecutive-ones-iii/ */
 
+/* Optimal Approach :- --> Its same like the shrinking window concept , find the valid condition for the window 
+ *                     --> Based on the condition expand or shrink the window 
+ *                     --> The condition for the window is " The window can have at most 2 zeros only"
+ *                     
+ *                     --> T.C :- O( 2n ) -> as at worst case both the pointers will be moving till the end of the window.
+ */
+
 
 public class max_consecutive_one_III {
 

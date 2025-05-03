ff5acbf28a2737011c70100f7a3dd811bb633d75
Mageshwaran <mageshwaranp18@gmail.com>
2025-05-03 18:15:36 +0530

    3.5.25

--- A_to_Z/Array/merge_intervals.java
+++ A_to_Z/Array/merge_intervals.java
@@ -10,9 +10,11 @@ import java.util.List;
 
 
 
+
+
 public class merge_intervals {
 
-    public static  int[][] merge(int[][] intervals) {
+    public static  int[][] better(int[][] intervals) {
             Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
 
             List<int[]> merged = new ArrayList<>();
@@ -35,11 +37,12 @@ public class merge_intervals {
 
     public static void main(String[] args) {
         int[][] intervals = {{1,3}, {2,6}, {8,10}, {15,18}};
-        int[][] result = merge(intervals);
+        int[][] result = better(intervals);
         
         System.out.println("Merged intervals:");
         for (int[] interval : result) {
             System.out.println(Arrays.toString(interval));
         }
+    
     }
 }
